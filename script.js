5a3170c9b3a6f95ec53319a79e272d32aaf4e549
Crystal <ceceherrera490@gmail.com>
2021-03-21 23:10:03 -0400

    adding some extra jS code and css

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My Very First Website";
 
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm So Excited!!";
+
+document.querySelector("body").appendChild(h3);
